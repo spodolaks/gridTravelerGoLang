d5884262e478273ce07b7e55b4b968050e05e718
Sergejs Podolaks <sergejspodolaks@Sergejss-MacBook-Pro-2.local>
2023-04-04 10:11:01 +0300

    init

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func gridTraveler(n int64, m int64) int64 {
+	if n == 1 && m == 1{
+		return 1
+	}
+	if (n == 0 || m == 0){
+		return 0
+	}
+	// if _, ok := memo[n]; !ok {
+	// 	memo[n] = fibonacci(n-1, memo) + fibonacci(n-2, memo)
+	// }
+
+	return gridTraveler(n-1,m) + gridTraveler(n,m-1)
+}
+
+func main() {
+	var n int64 = 16
+	var m int64 = 16
+
+	// memo := make(map[int64]int64)
+
+	// Start the timer
+	startTime := time.Now()
+
+	result := gridTraveler(n, m)
+
+	// Calculate the elapsed time in milliseconds
+	elapsedTimeMs := time.Since(startTime).Milliseconds()
+
+	fmt.Printf("For %d by %d grid number of ways: %d \n", n,m,result)
+	fmt.Printf("Execution time: %d ms\n", elapsedTimeMs)
+}
